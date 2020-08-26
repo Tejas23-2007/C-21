20aab807caaefcbe46e3305e4c5c85380d1a85f4
Tejas23-2007 <69716592+Tejas23-2007@users.noreply.github.com>
2020-08-26 17:06:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var fixedRect, movingRect; 
+var box1, box2
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  box1 = createSprite(1000,400,50,50);
+  box1.shapeColor= "red"
+  box2=createSprite (100,400,50,50)
+  box2.shapeColor= "blue"
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+  box1.velocityX = -5
+  box2.velocityX = +5
+
+}
+
+function draw() {
+  background(0,0,0);  
+
+ bounceOff(box1,box2);
+  
+  drawSprites();
+}
+
+
